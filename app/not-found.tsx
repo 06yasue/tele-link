b09736f95a24b14b66a36e8176d42f32db704c99
06yasue <101648951+06yasue@users.noreply.github.com>
2026-04-11 17:27:38 +0700

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,16 +6,16 @@ export default function NotFound() {
     <div className="flex min-h-screen flex-col items-center justify-center bg-zinc-950 text-white">
       <h1 className="text-9xl font-extrabold tracking-widest text-rose-600">404</h1>
       <div className="absolute rotate-12 rounded bg-rose-700 px-2 text-sm shadow-lg">
-        URL Tidak Ditemukan
+        URL Not Found
       </div>
       <p className="mt-8 text-center text-lg text-zinc-400">
-        Waduh, link yang dicari tidak ada atau sudah dihapus.
+        Oops, the link you are looking for doesn't exist or has been deleted.
       </p>
       <Link
         href={`https://${siteConfig.domain}`}
         className="mt-6 inline-block rounded bg-indigo-600 px-6 py-3 font-semibold text-white transition hover:bg-indigo-700 shadow-md"
       >
-        Kembali ke {siteConfig.name}
+        Back to {siteConfig.name}
       </Link>
     </div>
   );
